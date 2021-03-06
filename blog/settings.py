14e3bd1b09d82c92065a8064158801da36b61b33
Taras <shyichuk.taras@gmail.com>
2021-03-06 13:57:21 +0200

    fix11

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gw%e%@8^1xf553kkfw&0b2p8h2%2&%fieajwi)d@+i00iivj-y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dw-project.herokuapp.com']
 
