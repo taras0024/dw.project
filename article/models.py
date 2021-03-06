22324a73f233449a3abab1997d2dfa2886c97d04
Taras <shyichuk.taras@gmail.com>
2021-03-06 14:18:12 +0200

    fix12

--- article/models.py
+++ article/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    body = RichTextUploadingField()
+    body = RichTextUploadingField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=POST_STATUS, default='draft')
 
     created = models.DateTimeField(auto_now_add=True)
