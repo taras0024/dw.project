cbbd212cfb71f5d4481f076c9b26046f579f1b1d
Taras <shyichuk.taras@gmail.com>
2021-03-10 14:15:18 +0200

    0310

--- blog/settings.py
+++ blog/settings.py
@@ -156,10 +156,11 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGGING = {
     "version": 1,
--- parser.py
+++ parser.py
@@ -13,8 +13,14 @@ class Parser:
     def __init__(self, host):
         self.host = host
 
+    def get(self, url):
+        return requests.get(f'{self.host}{url}').json()
+
+    # def get_posts(self):
+    #     return requests.get(f'{self.host}{url_posts}').json()
+
     def get_posts(self):
-        return requests.get(f'{self.host}{url_posts}').json()
+        return self.get('api/article/posts/')
 
     def get_posts_top_by_like(self):
         return requests.get(f'{self.host}{url_posts_top_by_like}').json()
@@ -40,48 +46,49 @@ local = Parser('http://127.0.0.1:8000/')
 heroku = Parser('https://dw-project.herokuapp.com/')
 #
 # print('local')
-# # print(local.get_posts())
-# # print(local.get_posts_top_by_like())
-# # print(local.get_posts_top_by_comments())
-# # print(local.get_one_post(1202))
-# # print(local.do_like(1202))
-# # print(local.post_comment('1202'))
+print(local.get_posts())
+# print(local.get_posts_top_by_like())
+# print(local.get_posts_top_by_comments())
+# print(local.get_one_post(1202))
+# print(local.do_like(1202))
+# print(local.post_comment('1202'))
+
+# print('heroku')
+# print(heroku.get_posts())
+
+
+
+# while True:
+#     text = (
+#         "Posts \n"
+#         "Posts_top_by_like \n"
+#         "Posts_top_by_comments \n"
+#         "Post \n"
+#         "Like \n"
+#         "Comment \n"
+#         "End"
+#     )
+#     print(text)
+#     user_input = input('Виберіть операцію яку хочете виконати: ').lower()
 #
-# # print('heroku')
-# # print(heroku.get_posts())
+#     if user_input == "end":
+#         break
+#     elif user_input == 'posts':
+#         print(heroku.get_posts())
+#     elif user_input == 'posts_top_by_like':
+#         print(heroku.get_posts_top_by_like())
+#     elif user_input == 'posts_top_by_comments':
+#         print(heroku.get_posts_top_by_comments())
+#     elif user_input == 'post':
+#         print(heroku.get_one_post(input('Введіть ID поста: ')))
+#     elif user_input == 'like':
+#         print(heroku.do_like('Введіть ID поста якому хочете поставити лайк: '))
+#     elif user_input == 'comment':
+#         post_id = input('Введіть ID поста якому хочете залишити коментар: ')
+#         heroku.post_comment(post_id, {
+#             'author': input("Введіть ім\'я автора "),
+#             'body': input('Введіть текст поста: ')
+#         })
+#     else:
+#         print('Невірна операція \n')
 
-
-
-while True:
-    text = (
-        "Posts \n"
-        "Posts_top_by_like \n"    
-        "Posts_top_by_comments \n"
-        "Post \n"    
-        "Like \n"    
-        "Comment \n"
-        "End"
-    )
-    print(text)
-    user_input = input('Виберіть операцію яку хочете виконати: ').lower()
-
-    if user_input == "end":
-        break
-    elif user_input == 'posts':
-        print(heroku.get_posts())
-    elif user_input == 'posts_top_by_like':
-        print(heroku.get_posts_top_by_like())
-    elif user_input == 'posts_top_by_comments':
-        print(heroku.get_posts_top_by_comments())
-    elif user_input == 'post':
-        print(heroku.get_one_post(input('Введіть ID поста: ')))
-    elif user_input == 'like':
-        print(heroku.do_like('Введіть ID поста якому хочете поставити лайк: '))
-    elif user_input == 'comment':
-        post_id = input('Введіть ID поста якому хочете залишити коментар: ')
-        heroku.post_comment(post_id, {
-            'author': input("Введіть ім\'я автора "),
-            'body': input('Введіть текст поста: ')
-        })
-    else:
-        print('Невірна операція')
